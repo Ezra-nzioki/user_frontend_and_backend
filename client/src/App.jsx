3fb42f5fc21255a91c11b3a35c701d87613f2b73
Ezra Nzioki <135170744+Ezra-nzioki@users.noreply.github.com>
2026-02-25 15:36:01 +0000

    color

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,13 +7,15 @@ import Update from './components/update.jsx'
 function App() {
  
   return (
-    <>
-    <Routes>
-      <Route path='/' element={<UserTable />} />
-      <Route path='/add' element={<AddUser />} />
-      <Route path='/update/:id' element={<Update />} />
-    </Routes>
-    </>
+    <div className='bg-gradient-to-r from-indigo-500 to-teal-400'>
+        <Routes>
+          <Route path='/' element={<UserTable />} />
+          <Route path='/add' element={<AddUser />} />
+          <Route path='/update/:id' element={<Update />} />
+        </Routes>
+    </div>
+        
+    
    
      
   )
