f5095f2b8191a78e02e57c13e2921da73fe8df31
Ezra Nzioki <135170744+Ezra-nzioki@users.noreply.github.com>
2026-02-25 14:33:10 +0000

    last to update

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -32,6 +32,7 @@ export const getUser=async (req,res) => {
             return res.status(404).json({message:"User not found"})
         }
         res.status(200).json(user)
+        
     } catch (error) {
         res.status(500).json({message:"Error fetching user",error})
     }
@@ -50,12 +51,12 @@ export const updateUser=async (req,res) => {
         user.email=email || user.email
         user.country=country || user.country
         await user.save()
-        res.status(200).json(user)
-
+        // res.status(200).json(user)
+        res.status(201).json({message:"User updated successfully"})
 
     }
     catch (error) {
-        res.status(500).json({message:"Error fetching users",error})
+        res.status(500).json({message:"Error updating user",error})
     }
 }
 
